76fb7bf06dca05bc5a187dfbdf61d376769602ac
tiredhaydn <tiredhaydn@gmail.com>
2019-03-30 14:27:03 +0900

    エラーメッセージをほんのちょっと改善

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,11 @@ use befunge::Befunge;
 use std::env;
 
 fn main() -> Result<(), Box<std::error::Error>> {
-    let path = env::args().nth(1).unwrap();
-    let mut befunge = Befunge::new(path.as_str())?;
+    let path = env::args().nth(1);
+    if let None = path {
+        eprintln!("usage: befunge SOURCE");
+        return Ok(())
+    }
+    let mut befunge = Befunge::new(path.unwrap().as_str())?;
     befunge.run()
 }
